5b691e5abff942bbe7f032a7235d1b47cdf48bf4
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-01-31 23:58:36 -0300

    Add files via upload

---
+++ while4.py
@@ -0,0 +1,15 @@
+x = 0
+y = 0
+
+while x < 10:
+    y = 0
+
+    while y < 5: # A cada vez que y tiver valor 5, x valerá 1 e y voltará a valer 0
+        print("X =", x)
+        print("Y =", y)
+        print("")
+        y += 1
+
+    x += 1
+print("FIM")
+
