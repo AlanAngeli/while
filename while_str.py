bc315cd3203f5a63874d6a325776de217afd2cf8
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-01 11:26:03 -0300

    Add files via upload

---
+++ while_str.py
@@ -0,0 +1,9 @@
+frase = "Socorram-me subi no ônibus em Marrocos"
+tamanho_frase = len(frase)
+contador = 0
+
+# print(tamanho_frase) #38 caracteres
+
+while contador <= 37:
+    print(frase[contador], contador)
+    contador += 1
